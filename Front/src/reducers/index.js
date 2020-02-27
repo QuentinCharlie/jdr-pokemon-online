160d5d028002ee6b841c801deb4801c44e7799f2
Florent - O'Clock <florent-oclock@MacBook-Pro-de-Florent.local>
2020-02-27 13:03:42 +0100

    log component styled

--- Front/src/reducers/index.js
+++ Front/src/reducers/index.js
@@ -8,3 +8,25 @@ const rootReducer = combineReducers({
 });
 
 export default rootReducer;
+
+
+const pokemonPalette = {
+  bulbizarre: [
+    '#62d5b4',
+    '#83eec5',
+    '#184a4a',
+  ],
+  herbizarre: [
+    '#5acdbd',
+    '#317b52',
+    '#104a39',
+  ],
+  florizzare: [
+    '#105241',
+    '#ff7b73',
+    '#107b6a',
+  ],
+}
+
+// faut pas les mettre dans le state les couleurs ;)
+// Mais dans le theme :p
