cf1aa99751cdf9db97667b4c1664dec700033d7b
Florent - O'Clock <florent-oclock@MacBook-Pro-de-Florent.local>
2020-02-27 14:58:26 +0100

    trainer styled componenet

--- Front/src/components/Trainer/TrainerStyled.js
+++ Front/src/components/Trainer/TrainerStyled.js
@@ -13,6 +13,40 @@ const TrainerStyled = styled.div`
     height: calc(30vh - 40px - 3em);
     margin: 1em;
     background-color: lightgray;
+    padding: .5em;
+    position: relative;
+    .trainer-avatar {
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+      position: relative;
+      .health-container {
+        position: absolute;
+        bottom: 0em;
+        height: 1.5em;
+        width: 100%;
+        text-align: center;
+        background-color: gray;
+        .health-bar {
+          position: absolute;
+          bottom: 0em;
+          height: 100%;
+          background-color: green;
+          span {
+            position: absolute;
+            left: 100%;
+            transform: translateX(-50%);
+          }
+        }
+      }
+    }
+    .money {
+      position: absolute;
+      top: .5em;
+      right: .5em;
+      background-color: yellow;
+      padding: .2em .5em;
+    }
   }
 `;
 
