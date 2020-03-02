5c6a01ae0ae137549105851195f3d803ae24aee1
Florent Guiliana <florent.guiliana@gmail.com>
2020-03-02 12:25:28 +0100

    dices component styled

--- Front/src/components/Dices/DicesStyled.js
+++ Front/src/components/Dices/DicesStyled.js
@@ -12,7 +12,71 @@ const DicesStyled = styled.div`
     width: calc(30vw - 2em);
     height: calc(30vh - 40px - 3em);
     margin: 1em 1em 1em 0;
-    background-color: lightgray;
+    /* background-color: lightgray; */
+    .dices {
+      display: flex;
+      justify-content: space-between;
+      width: 100%;
+      height: 100%;
+    }
+    .dice {
+      display: flex;
+      background: linear-gradient(180deg, lightgray 0%, rgba(255,255,255,0) 28%);
+      flex-grow: 1;
+      flex-direction: column;
+      justify-content: space-between;
+      height: 100%;
+      width: calc((100% / 3) - .2em );
+      img {
+        object-fit: contain;
+        height: 48%;
+        margin-bottom: .6em;
+      }
+      button {
+        background: linear-gradient(0deg, rgba(240,0,0,1) 0%, rgba(238,21,21,1) 100%);
+        border-bottom: 2px solid black;
+        border-radius: 5px;
+        color: white;
+        font-weight: bold;
+        box-shadow: 0 10px 0 lightgrey;
+        height: calc(48% - 10px);
+        margin-bottom: 10px;
+        position: relative;
+        transition: .2s;
+        span {
+          background-color: white;
+          width: 10px;
+          height: 10px;
+          border: solid 2px black;
+          border-radius: 50px;
+          position: absolute;
+          bottom: -6px;
+          left: calc(50% - 5px);
+        }
+        &:active {
+          background: linear-gradient(0deg, rgba(238,83,83,1) 0%, rgba(240,0,0,1) 100%);
+          transform: translateY(10px);
+          box-shadow: none;
+          transition: .2s;
+        }
+      }
+    }
+    .dice:first-child {
+      margin-right: .4em;
+    }
+    .dice:last-child {
+      margin-left: .4em;
+    }
+    .dice-trainer {
+      /* background-color: lightblue; */
+    }
+    .dice-pokemon {
+      /* background-color: red; */
+    }
+    .dice-luck {
+      /* background-color: lightgreen; */
+    }
+   
   }
 `;
 
--- Front/src/components/Dices/index.js
+++ Front/src/components/Dices/index.js
@@ -1,13 +1,35 @@
+/* eslint-disable react/button-has-type */
 // == Import npm
 import React from 'react';
 
 // Styles
 import DicesStyled from './DicesStyled';
 
+// == Import files
+import ashTrainer from './sacha-face.png';
+
 
 // == Composant
 const Dices = () => (
-  <DicesStyled />
+  <DicesStyled>
+    <div className="dices">
+      <div className="dice dice-trainer">
+        <img src={ashTrainer} alt="" />
+        <button>Dés Dresseur<span /></button>
+      </div>
+      <div className="dice dice-pokemon">
+        <img src="https://img.pokemondb.net/sprites/black-white/normal/abra.png" alt="" />
+        <button>Dés Pokémon<span /></button>
+      </div>
+      <div className=" dice dice-luck">
+        <img src="https://img.pokemondb.net/sprites/black-white/normal/chansey.png" alt="" />
+        <button>Roll Chance<span /></button>
+      </div>
+    </div>
+    <div className="trainer-list-dices" />
+    <div className="pokemon-list-dices" />
+
+  </DicesStyled>
 );
 
 // == Export
