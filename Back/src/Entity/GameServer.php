736209af67a0e176535fcbd3133d6bfa51616874
DesbordesDylan <desbordesdylan@yahoo.fr>
2020-03-12 14:10:39 +0100

    headers fix again

---
+++ Back/src/Entity/GameServer.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ */
+class GameServer
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $port;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $MJ;
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of port
+     */ 
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+    /**
+     * Set the value of port
+     *
+     * @return  self
+     */ 
+    public function setPort($port)
+    {
+        $this->port = $port;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of MJ
+     */ 
+    public function getMJ()
+    {
+        return $this->MJ;
+    }
+
+    /**
+     * Set the value of MJ
+     *
+     * @return  self
+     */ 
+    public function setMJ($MJ)
+    {
+        $this->MJ = $MJ;
+
+        return $this;
+    }
+} 
