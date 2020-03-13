3c28b13fdf88351c2e822b4369ffec54b471e5f2
QuentinCharlie <quentin.charlie@orange.fr>
2020-03-13 21:58:25 +0100

    pokemon card in selector

--- Front/src/components/PokemonSelector/PokemonSelectorStyled.js
+++ Front/src/components/PokemonSelector/PokemonSelectorStyled.js
@@ -1,5 +1,187 @@
 import styled from 'styled-components';
 
-const PokemonSelectorStyled = styled.div``;
+const PokemonSelectorStyled = styled.div `
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .loading {
+  }
+  .pokemon-list {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    margin: .5em 0 0 .5em;
+    .pokemon-card {
+      width: calc((100% / 3) - .5em);
+      margin: 0 .5em .5em 0;
+      color: white;
+      border-radius: 10px;
+      box-shadow: 0 0 15px rgba(117, 117, 117, .6);
+      padding: 1em;
+      display: flex;
+      .pokemon-avatar {
+        height: calc(30vh - 40px - 4em);
+        width: calc(30vh - 40px - 4em);
+        margin-right: 1em;
+        padding-bottom: 3em;
+        position: relative;
+        .avatar-container {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          padding: .5em;
+          width: 100%;
+          height: 100%;
+          cursor: pointer;
+          img {
+            max-height: 100%;
+            max-width: 100%;
+            -webkit-filter: drop-shadow(0px 0px 15px rgba(117, 117, 117, .3));
+            filter: drop-shadow(0px 0px 15px rgba(117, 117, 117, .3));
+          }
+        }
+        /* BARRE GRISE */
+        .energy-container {
+          position: absolute;
+          bottom: 1.8em;
+          height: 1.5em;
+          width: 100%;
+          background-color: rgba(20, 20, 20, 0.05);
+          border-radius: 10px;
+          text-align: center;
+          span {
+            color: white;
+            height: 100%;
+            position: relative;
+            top: calc(50% - .7em);
+            z-index: 5;
+          }
+          img {
+            position: absolute;
+            z-index: 5;
+            left: .3em;
+            bottom: .15em;
+            height: 80%;
+          }
+          /* BARRE VERTE*/
+          .energy-bar {
+            position: absolute;
+            bottom: 0em;
+            height: 100%;
+            background-color: #98bfe6;
+            border-radius: 10px 0 0 10px;
+          }
+        }
+        /* BARRE GRISE */
+        .health-container {
+          position: absolute;
+          bottom: 0;
+          height: 1.5em;
+          width: 100%;
+          background-color: rgba(20, 20, 20, 0.05);
+          border-radius: 10px;
+          text-align: center;
+          span {
+            color: white;
+            height: 100%;
+            position: relative;
+            top: calc(50% - .7em);
+            z-index: 5;
+          }
+          img {
+            position: absolute;
+            z-index: 5;
+            left: .3em;
+            bottom: .15em;
+            height: 80%;
+          }
+          /* BARRE VERTE*/
+          .health-bar {
+            position: absolute;
+            bottom: 0em;
+            height: 100%;
+            background-color: #19cd70;
+            border-radius: 10px 0 0 10px;
+          }
+        }
+      }
+        .exp {
+          /* font-weight: bold; */
+          color: white;
+          text-shadow: 1px 1px 2px rgba(20, 20, 20, .15);
+          position: absolute;
+          top: 0em; left: 0;
+          background-color: rgba(20, 20, 20, 0.05);
+          border-radius: 10px;
+          padding: .2em .5em;
+        }
+
+
+        .pokemon-info {
+          width: calc(100% - (30vh - 40px - 4em));
+          height: 100%;
+          /* padding: 0.5em; */
+          overflow: auto;
+          /* scrollbar hidden (IE/Edge) */
+          -ms-overflow-style: none;
+          /* scrollbar hidden (Chrome/Firefox/Safari) */
+          &::-webkit-scrollbar {
+            display: none;
+          }
+          p {
+            margin-bottom: 0;
+          }
+          .pokemon-presentation {
+            display: flex;
+            flex-direction: column;
+            margin-bottom: .6em;
+            .pokemon-name {
+              text-shadow: 1px 1px 2px rgba(20, 20, 20, .15);
+              font-size: 2em;
+              font-weight: bold;
+            }
+            .pokemon-types {
+              margin-top: .5em;
+              .pokemon-type {
+                &:first-child {
+                  margin-right: .3em;
+                }
+                border-radius: 10px;
+                font-size: .8em;
+                display: inline-block;
+                padding: .07em .3em;
+                font-style: italic;
+                background-color: rgba(20, 20, 20, 0.05);
+
+                /* @todo one background-color per type (x16) */
+              }
+            }
+          }
+          .pokemon-stats {
+            width: 100%;
+            /* height: 100%; */
+          .stat {
+            display: flex;
+            align-items: flex-end;
+            .title {
+              color: white;
+              padding: .2em 0;
+              width: 30%;
+              text-shadow: 1px 1px 2px rgba(20, 20, 20, .15);
+              /* text-align: center; */
+              abbr {
+                font-style: none;
+                text-decoration: none;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default PokemonSelectorStyled;
--- Front/src/components/PokemonSelector/index.js
+++ Front/src/components/PokemonSelector/index.js
@@ -5,6 +5,15 @@ import PropTypes from 'prop-types';
 // == Import style
 import PokemonSelectorStyled from './PokemonSelectorStyled';
 
+// == Import utils
+import { findPokemonImage } from 'src/utils/functions';
+
+// == Import Components
+import ProgressBar from 'src/components/ProgressBar';
+
+// == Import files
+import heartIcon from 'src/assets/images/heart.svg';
+
 // == Composant
 const PokemonSelector = ({
   pokemons,
@@ -15,27 +24,75 @@ const PokemonSelector = ({
   useEffect(() => {
     loadAllPokemons();
   }, []);
-
+  
   const handleClick = (e)  => {
-    const pokemonName = e.target.dataset.name;
+    const pokemonName = e.currentTarget.dataset.name;
     const pokemon = pokemons.find((pokemon) => pokemon.name === pokemonName);
     linkPokemonToUser(pokemon);
   }
+
+  let pokemonMaxHealth = 125;
   
   return (
     <PokemonSelectorStyled>
+    {pokemons === undefined && <div className="loading">Chargement des Pokémon...</div>}
     {pokemons !== undefined && 
-      <>
+      <div className="pokemon-list">
         {pokemons.map((pokemon) => (
-          <div
+          <div 
+            className="pokemon-card"
+            style={{ backgroundColor: `#${pokemon.primary}` }}
             key={pokemon.id}
             onClick={handleClick}
             data-name={pokemon.name}
           >
-            {pokemon.name}
+            <div className="pokemon-avatar">
+                <div className="avatar-container">
+                  <img src={findPokemonImage(`${pokemon.id}.png`)} alt="avatar pokemon" />
+                </div>
+              <div className="health-container">
+                <span>{pokemon.vitality}</span>
+                <img src={heartIcon} alt="heart logo" />
+                <div className="health-bar" style={{ width: `${Math.ceil(pokemon.vitality / pokemonMaxHealth * 100)}%` }} />
+              </div>
+            </div>
+            <div className="pokemon-info">
+              <div className="pokemon-presentation">
+                <span className="pokemon-name">{pokemon.name}</span>
+                <div className="pokemon-types">
+                  {pokemon.types.map((type) => (
+                    <span className="pokemon-type" key={type.id}>
+                      {type.name}
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <div className="pokemon-stats">
+                <div className="stat">
+                  <span className="title"><abbr title="FORCE">FOR</abbr></span>
+                  <ProgressBar color="#f8cc53" number={pokemon.strength} maxNumber={16} />
+                </div>
+                <div className="stat">
+                  <span className="title"><abbr title="ENDURANCE">END</abbr></span>
+                  <ProgressBar color="#f8cc53" number={pokemon.endurance} maxNumber={16} />
+                </div>
+                <div className="stat">
+                  <span className="title"><abbr title="CONCENTRATION">CON</abbr></span>
+                  <ProgressBar color="#f8cc53" number={pokemon.concentration} maxNumber={16} />
+                </div>
+                <div className="stat">
+                  <span className="title"><abbr title="VOLONTE">VOL</abbr></span>
+                  <ProgressBar color="#f8cc53" number={pokemon.willpower} maxNumber={16} />
+                </div>
+                <div className="stat">
+                  <span className="title"><abbr title="DEXTERITE">DEX</abbr></span>
+                  <ProgressBar color="#f8cc53" number={pokemon.dexterity} maxNumber={16} />
+                </div>
+              </div>
+            </div>
           </div>
         ))}
-      </>
+      </div>
     }
     </PokemonSelectorStyled>
   );
