6eb7ff62e2a720dc29a5b2f92f28075cbfbb1c05
QuentinCharlie <quentin.charlie@orange.fr>
2020-03-14 19:51:57 +0100

    shine adjust

--- Front/src/components/PokemonSelector/PokemonSelectorStyled.js
+++ Front/src/components/PokemonSelector/PokemonSelectorStyled.js
@@ -225,16 +225,16 @@ const PokemonSelectorStyled = styled.div`
       z-index: 2;
       display: block;
       content: '';
-      width: 50%;
+      width: 40%;
       height: 100%;
-      background: -webkit-linear-gradient(left, rgba(255,255,255,0) 0%, rgba(255,255,255,.3) 100%);
-      background: linear-gradient(to right, rgba(255,255,255,0) 0%, rgba(255,255,255,.3) 100%);
+      background: -webkit-linear-gradient(left, rgba(255,255,255,0) 0%, rgba(255,255,255,.4) 100%);
+      background: linear-gradient(to right, rgba(255,255,255,0) 0%, rgba(255,255,255,.4) 100%);
       -webkit-transform: skewX(-25deg);
       transform: skewX(-25deg);
     }
     .shiny-effect:hover::before {
-      -webkit-animation: shine .75s;
-      animation: shine .75s;
+      -webkit-animation: shine .7s;
+      animation: shine .7s;
     }
     @-webkit-keyframes shine {
       100% {
