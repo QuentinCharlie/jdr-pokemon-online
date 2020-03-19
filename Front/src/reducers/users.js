2818a76da1d1b6318f61a535e9f1ab3f366b6d9d
Florent Guiliana <florent.guiliana@gmail.com>
2020-03-19 13:19:33 +0100

    build to dist

--- Front/src/reducers/users.js
+++ Front/src/reducers/users.js
@@ -9,7 +9,6 @@ const initialState = {
 // Reducer
 const usersReducer = (state = initialState, action = {}) => {
   console.log('usersReducer');
-  // let user = document.querySelector('#root').dataset.user;
 
   switch (action.type) {
     case UPDATE_USERS_STATE: {
