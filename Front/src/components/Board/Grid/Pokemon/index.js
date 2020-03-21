49540eb0ac1b99b7fac521850aec95b961f4c925
QuentinCharlie <quentin.charlie@orange.fr>
2020-03-21 18:56:59 +0100

    drag image + touch mobile + save position when new player load in the game

--- Front/src/components/Board/Grid/Pokemon/index.js
+++ Front/src/components/Board/Grid/Pokemon/index.js
@@ -1,7 +1,7 @@
 // == Import npm
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useDrag } from 'react-dnd';
+import { DragPreviewImage, useDrag } from 'react-dnd';
 
 // Import Utils
 import { findPokemonImage } from 'src/utils/functions';
@@ -12,29 +12,44 @@ import { ItemTypes } from './Constants';
 
 // == Composant
 const Pokemon = ({ userTrainer, pokemonTrainer, pokemon, X, Y }) => {
-  const [{ isDragging }, drag] = useDrag({
+  const [{ isDragging }, drag, preview] = useDrag({
     item: { type: ItemTypes.POKEMON },
     collect: (monitor) => ({
       isDragging: !!monitor.isDragging(),
     }),
   });
+
+
   if (userTrainer.name === pokemonTrainer.name) {
     return (
-      <img
-        ref={drag}
-        className={pokemon.name}
-        src={findPokemonImage(`${pokemon.id}.gif`)}
-        alt=""
-        style={{
-          height: '49px',
-          left: `calc( (50 * ${X}px) - 50px )`,
-          top: `calc( (50 * ${Y}px) - 50px )`,
-          position: 'absolute',
-          zIndex: 50,
-          opacity: isDragging ? 0.5 : 1,
-          cursor: 'move',
-        }}
-      />
+      <>
+        <DragPreviewImage connect={preview} src={findPokemonImage(`${pokemon.id}drag.png`)} />
+        <div
+          ref={drag}
+          style={{
+            height: '49px',
+            position: 'absolute',
+            cursor: 'move',
+            zIndex: 50,
+            left: isDragging ? `calc( (50 * ${X}px) - 50px )`:`calc( (50 * ${X}px) - 50px )`,
+            top: isDragging ? `calc( (50 * ${Y}px) - 50px )` :`calc( (50 * ${Y}px) - 50px )`,
+            opacity: isDragging ? 0.5 : 1,
+          }}
+        >
+          <img
+            className={pokemon.name}
+            src={findPokemonImage(`${pokemon.id}.gif`)}
+            alt=""
+            style={{
+              height: '49px',
+              left: `calc( (50 * ${X}px) - 50px )`,
+              top: `calc( (50 * ${Y}px) - 50px )`,
+              zIndex: 50,
+              cursor: 'move',
+            }}
+          />
+        </div>
+      </>
     );
   }
   return (
